36aa2ea105875f9213015af544be5e62de3513a6
Brilliant Makanju <brilliantmakanju4@gmail.com>
2024-07-09 21:07:36 -0400

    (feat) - Completed Dashboard ui with job posting popup, mobile sidebar with responsive popup

--- components/custom/navigation/nav_mobile_sidebar.tsx
+++ components/custom/navigation/nav_mobile_sidebar.tsx
@@ -9,7 +9,7 @@ const Nav_Mobile_Sidebar = ({ open }: NavSideMobile) => {
 
     return (
         <header className={`flex flex-col md:hidden justify-start items-start absolute h-full top-[57px] ${open ? 'w-auto left-0 ' : 'w-auto left-[-100%]' } overflow-hidden transition-all ease-in-out duration-300 z-[2] `}>
-            <aside className={`h-[94%] transition-all ease-in-out duration-300 w-[300px] overflow-hidden `}>
+            <aside className={`h-[94%] transition-all ease-in-out duration-300 w-[300px] overflow-hidden pb-[13px] `}>
                 <nav className="h-full flex flex-col bg-white w-auto border-l-0 border-r pt-[10px]" >
                     <div className='h-full flex flex-col justify-between items-start'>
                         <ul className=" flex flex-col justify-start items-start gap-[10px] p-3">
